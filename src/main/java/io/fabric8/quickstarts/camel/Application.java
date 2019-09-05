2b67b19e60a543617fcad13c9babc5a60fd63d82
Hernan Castro Rodriguez <hernan.castro@stcomputacion.cl>
2019-09-05 09:43:40 -0400

    CHANGE JAVA DSL TO XML DSL

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -15,7 +15,6 @@
  */
 package io.fabric8.quickstarts.camel;
 
-import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -30,19 +29,14 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
-public class Application extends RouteBuilder {
+public class Application {
+//	public class Application extends RouteBuilder {
 
     // must have a main method spring-boot can run
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    // @Override
-    // public void configure() throws Exception {
-    //     from("timer://foo?period=5000")
-    //         .setBody().constant("Hello World")
-    //         .log(">>> ${body}");
-    // }
     @Bean
     ServletRegistrationBean servletRegistrationBean() {
         ServletRegistrationBean servlet = new ServletRegistrationBean(
@@ -50,4 +44,10 @@ public class Application extends RouteBuilder {
         servlet.setName("CamelServlet");
         return servlet;
     }
+    // @Override
+    // public void configure() throws Exception {
+    //     from("timer://foo?period=5000")
+    //         .setBody().constant("Hello World")
+    //         .log(">>> ${body}");
+    // }
 }
